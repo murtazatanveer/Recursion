472c22fad04d85e921e7d493c132caa2e61a0e6d
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-11-20 22:11:02 +0500

    changes

---
+++ triangular number using recursion.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void sum();
+
+int main(){
+
+int num;
+
+printf("\nEnter number you want to find Triangular Number : ");
+
+scanf("%d",&num);
+sum(num);
+
+}
+
+void sum(int num){
+
+
+
+if(num==0){
+
+    return;
+}
+
+
+int i =sum(num-1);
+printf("\nTriangular Number is : %d ",s);
+
+
+}
